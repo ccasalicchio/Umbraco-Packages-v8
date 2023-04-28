184eebca4beca3c482d32c403da64235b1de56f4
Eliana Calderón <eliana.calderon@opnsolutions.com>
2023-04-28 11:13:12 -0300

    Controller in copy Value

--- Umbraco.Cms.Web.v8.18.7/App_Plugins/CopyValue/js/controller.js
+++ Umbraco.Cms.Web.v8.18.7/App_Plugins/CopyValue/js/controller.js
@@ -1,58 +1,22 @@
 angular.module("umbraco").controller("splatDev.CopyValue.Controller", function ($scope, $routeParams, contentEditingHelper, editorState) {
-    'use strict';
-    const vm = this;
-    vm.from = $scope.model.config.from.split(",");
-    vm.to = $scope.model.config.to.split(",")
+	'use strict';
+	const vm = this;
+	vm.from = $scope.model.config.from.split(",");
+	vm.to = $scope.model.config.to.split(",")
+	let content = editorState.getCurrent();
+	let currVariant = $routeParams.cculture;
+	let curTabs = currVariant !== undefined ? content.variants.find(x => x.language == currVariant) : content.variants[0];
+	let properties = contentEditingHelper.getAllProps(curTabs);
+	vm.done = false;
 
-    var languages = $scope.model.languages;
-   
-    if (languages == null) {
-        let content = editorState.current.variants.find(x => x.language === $routeParams.mculture);
-        let properties = contentEditingHelper.getAllProps(content);
-        vm.done = false;
+	vm.copy = function () {
+		for (var i = 0; i < vm.from.length; i++) {
+			_.findWhere(properties, { alias: vm.to[i] }).value = _.findWhere(properties, { alias: vm.from[i] }).value;
+		}
 
-        vm.copy = function () {
-            for (var i = 0; i < vm.from.length; i++) {
-                _.findWhere(properties, { alias: vm.to[i] }).value = _.findWhere(properties, { alias: vm.from[i] }).value;
-            }
-
-            vm.done = true;
-            setTimeout(function () {
-                vm.done = false;
-            }, 3 * 1000);
-    }
-
-    /*if ($scope.model.language != null) {
-        let content = editorState.current.variants.find(x => x.language.culture === $routeParams.mculture);
-        let properties = contentEditingHelper.getAllProps(content);
-        vm.done = false;
-
-        vm.copy = function () {
-            for (var i = 0; i < vm.from.length; i++) {
-                _.findWhere(properties, { alias: vm.to[i] }).value = _.findWhere(properties, { alias: vm.from[i] }).value;
-            }
-
-            vm.done = true;
-            setTimeout(function () {
-                vm.done = false;
-            }, 3 * 1000);
-        };*/
-    } else {
-        let content = editorState.current;
-        let properties = contentEditingHelper.getAllProps(content);
-        vm.done = false;
-
-        vm.copy = function () {
-            for (var i = 0; i < vm.from.length; i++) {
-                _.findWhere(properties, { alias: vm.to[i] }).value = _.findWhere(properties, { alias: vm.from[i] }).value;
-            }
-
-            vm.done = true;
-            setTimeout(function () {
-                vm.done = false;
-            }, 3 * 1000);
-        };
+		vm.done = true;
+		setTimeout(function () {
+			vm.done = false;
+		}, 3 * 1000);
 	}
-        
-   
 });
