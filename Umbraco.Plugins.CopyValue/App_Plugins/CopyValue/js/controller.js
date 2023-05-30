710a3b3f4a3909bd7fc05525afa160470f1fdcf5
Carlos A. A. Casalicchio <carlos.casalicchio@gmail.com>
2023-05-30 18:33:54 -0300

    migrated copy value to v8

--- Umbraco.Plugins.CopyValue/App_Plugins/CopyValue/js/controller.js
+++ Umbraco.Plugins.CopyValue/App_Plugins/CopyValue/js/controller.js
@@ -1,10 +1,15 @@
-angular.module("umbraco").controller("splatDev.CopyValue.Controller", function ($scope, $routeParams, contentEditingHelper, editorState) {
+angular.module("umbraco").controller("splatDev.CopyValue.Controller", function ($scope, contentEditingHelper, editorState) {
     'use strict';
     const vm = this;
     vm.from = $scope.model.config.from.split(",");
     vm.to = $scope.model.config.to.split(",")
 
-    let content = editorState.current.variants(x => x.language != null);
+    vm.display = {
+        from: [],
+        to: []
+    }
+
+    let content = editorState.current.variants.length === 1 ? editorState.current.variants[0] : editorState.current.variants(x => x.language != null);
     let properties = contentEditingHelper.getAllProps(content);
     vm.done = false;
 
@@ -19,4 +24,10 @@ angular.module("umbraco").controller("splatDev.CopyValue.Controller", function (
         }, 3 * 1000);
     };
 
+    vm.$onInit = function () {
+        for (var i = 0; i < vm.from.length; i++) {
+            vm.display.from.push(_.findWhere(properties, { alias: vm.from[i] }).label);
+            vm.display.to.push(_.findWhere(properties, { alias: vm.to[i] }).label);
+        }
+    }
 });
--- Umbraco.Plugins.CopyValue/README.md
+++ Umbraco.Plugins.CopyValue/README.md
@@ -3,10 +3,15 @@
 This Property Editor is a simple copy button that copies one or many values from a property into another property. Useful when you have more than one property with the same purpose, but that may or may not be the same value. Minimizes duplication of work. Ex. When you want an article title to replicate into the meta title.
 
 ![Imgur](https://i.imgur.com/r0nhzcy.png)
-![Imgur](https://i.imgur.com/GbnMpJA.png)
-![Imgur](https://i.imgur.com/VOpyFAB.png)
+![Imgur](https://i.imgur.com/zgrmBHg.png)
+![Imgur](https://i.imgur.com/YohxM2R.png)
+![Imgur](https://i.imgur.com/MSem60p.png)
 
-##### Umbraco v7.15.10
+##### Multiple Properties
+![Imgur](https://i.imgur.com/O7wpwRq.png)
+![Imgur](https://i.imgur.com/dTGpJiG.png)
+
+##### Umbraco v8.18.8
 
 - Creates a button that copies values from one (or several) property into another. The values are copied following the same index of the string array.
 - The Required Fields are 'From' and 'To', which can be multiple properties, comma separated
